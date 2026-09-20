8c228eb1d8a066db2cd9297aba1245f64bf27cd3
Wesley Johnson <wesley22johnson@gmail.com>
2026-09-20 08:22:14 -0300

    Use PORT environment variable

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const { db, nextTrackingNumber } = require('./db');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const JWT_SECRET = 'swiftship_secret_2026';
 
 app.use(express.json());
